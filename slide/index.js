309ec29ed4617083d9319da68458dbb79ed9879a
anton-orlov <42201531+anton-orlov@users.noreply.github.com>
2019-06-14 14:40:45 +0300

    Update index.js

--- slide/index.js
+++ slide/index.js
@@ -7,32 +7,3 @@
   });
 </script>
 
-<script>
-$(function() {
-  $("#getData").click(function() {
-  
-    // Put artistList element and JSON file location into a variable
-    var artistList = $("#artistList");
-    var url = "https://www.quackit.com/jquery/examples/artists.txt";
-
-    // Get the JSON file
-    $.getJSON(url, function(data) {
-
-      // Put artist info into a variable
-      var artists = data.artists.map(function(item) {
-        return item.artistname + " (" + item.born + ")";
-      });
-      
-      // Remove all child nodes (including text nodes) 
-      artistList.empty();
-
-      // Format artists with HTML tags 
-      if (artists.length) {
-        var content = "<li>" + artists.join("</li><li>") + "</li>";
-        var list = $("<ul>").html(content);
-        artistList.append(list);
-      }
-    });
-  });
-});
-</script>
