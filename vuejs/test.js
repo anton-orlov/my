51f950ac5f32b60c1a398705dc441eaec372820c
anton-orlov <42201531+anton-orlov@users.noreply.github.com>
2019-07-11 11:50:39 +0300

    Update test.js

--- vuejs/test.js
+++ vuejs/test.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(){ 
   // Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
