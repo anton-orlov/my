a72ea89a9ae75b0d1f3a8c006c590a91e5edeaa2
anton-orlov <42201531+anton-orlov@users.noreply.github.com>
2019-07-15 19:46:24 +0300

    Update script.js

--- bootstrap/src/script.js
+++ bootstrap/src/script.js
@@ -33,5 +33,5 @@ $(document).ready(function(){
 })
 
 $(window).scroll(function(){
-    $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 250);
+    $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 650);
   });
